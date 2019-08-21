79af8ac3bfbc5ba6821c0acc7fcd40ec56f98247
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-21 16:07:07 +0000

    Writing tests to create new card and double every second digit

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,3 +1,40 @@
  class Luhn
-    # add your implementation here
+
+    def initialize(arr)
+    end
+
+
+    #for each even index, replace val with doubled val
+    def double_digits(arr)
+
+      arr.each_with_index do |val, index|
+        if (index %2 == 0)
+          doubled_digit = val * 2
+          doubled_digit = arr[arr.index(val)]
+        end
+      end
+      puts arr
+    end
+
+    #for each index, if val>10, subtract 9 and store new val
+    #def compare_num(arr)
+      #reversed_num.each_with_index do |val, index|
+        #if val > 10
+          #changed_num = val - 9
+          #changed_num = reversed_num[reversed_num.index(val)]
+        #end
+      #end
+    #end
+    
+    #if sum of all digits is divisible by 10, render valid 
+    #def test_validity(arr)
+      #sum all values
+      #divide by 10
+      #if /10 = 0
+        #puts "Success!"
+        #else
+        #puts "Invalid Card Number"
+      #end
+    #end
+
   end
--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -1,6 +1,31 @@
 require 'spec_helper'
   require_relative '../lib/luhn'
 
-  describe Luhn do
-    # add your test examples here
+describe Luhn do
+
+  it "should take the value of a credit card" do
+    my_card = Luhn.new([4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
+    expect(my_card).to eq([4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
   end
+
+  it "should double the value of each even index" do
+    my_card = Luhn.new([4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
+    expect(my_card.value).to eq([8,1,18,4,10,6,0,3,16,5,0,0,16,5,0,5])
+  end
+
+  it "should adjust values if val>10" do 
+   
+  end
+
+   it "should test for valid card number" do 
+    
+  end
+
+
+end
+    
+
+  
+
+
+   
