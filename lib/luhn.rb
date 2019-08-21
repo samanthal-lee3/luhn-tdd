d3556ac025f74156e3103918b2068f0e8301a8c7
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-21 17:28:16 +0000

    Building working logic

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,40 +1,49 @@
  class Luhn
 
+
+
     def initialize(arr)
+      @luhn = arr
     end
 
 
     #for each even index, replace val with doubled val
-    def double_digits(arr)
+    def double_digits
+      @doubled = []
 
-      arr.each_with_index do |val, index|
+      @luhn.each_with_index do |val, index|
         if (index %2 == 0)
           doubled_digit = val * 2
-          doubled_digit = arr[arr.index(val)]
+          @doubled << doubled_digit
+        else
+          @doubled << val
         end
       end
-      puts arr
     end
 
+
     #for each index, if val>10, subtract 9 and store new val
-    #def compare_num(arr)
-      #reversed_num.each_with_index do |val, index|
-        #if val > 10
-          #changed_num = val - 9
-          #changed_num = reversed_num[reversed_num.index(val)]
-        #end
-      #end
-    #end
+    def compare_num
+      @subtracted = []
+
+      doubled.each_with_index do |val, index|
+        if val > 10
+          changed_num = val - 9
+          @subtracted << changed_num
+        else
+          @subtracted << val
+        end
+      end
+    end
     
-    #if sum of all digits is divisible by 10, render valid 
-    #def test_validity(arr)
-      #sum all values
-      #divide by 10
-      #if /10 = 0
-        #puts "Success!"
-        #else
-        #puts "Invalid Card Number"
-      #end
-    #end
+    def test_validity
+     card_sum = @subtracted.sum
+     if 
+        card_sum %10 == 0
+        puts "Success!"
+     else
+        puts "Invalid Card Number- Try Again"
+      end
+    end
 
   end
