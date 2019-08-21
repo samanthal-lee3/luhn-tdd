204fffe8fe9111b1d1af945de2b6a204a3b05fe8
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-21 17:59:47 +0000

    Add arrange act and assert

--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -4,24 +4,48 @@ require 'spec_helper'
 describe Luhn do
  
   it "should take the value of a credit card" do
+    #arrange
     my_card = Luhn.new([4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
+
+    #act
+
+    #assert
     expect(my_card).to eq(my_card)
   end
 
   it "should double the value of each even index" do
+    #arrange
     my_card = Luhn.new([4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
-    doubled = []
-    expect(my_card.double_digits).to eq([8,1,18,4,10,6,0,3,16,5,0,0,16,5,0,5])
+
+    #act
+    my_card.double_digits
+
+    #assert
+    expect(my_card).to eq([8,1,18,4,10,6,0,3,16,5,0,0,16,5,0,5])
   end
 
   it "should adjust values if val>=10" do 
-   my_card = Luhn.new([8,1,18,4,10,6,0,3,16,5,0,0,16,5,0,5])
+    #arrange
+    my_card = Luhn.new([[4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
 
+    #act
+    my_card == my_card.double_digits
+    my_card == my_card.compare_num
+
+    #assert
    expect(my_card).to eq([8,1,9,4,1,6,0,3,7,5,0,0,7,5,0,5])
   end
 
   it "should test for valid card number" do 
-    my_card = Luhn.new([8,1,9,4,1,6,0,3,7,5,0,0,7,5,0,5])
+    #arrange
+    my_card = Luhn.new([[4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
+
+    #act
+    my_card == my_card.double_digits
+    my_card == my_card.compare_num
+    my_card == my_card.test_validity
+
+    #assert
     expect(my_card).to eq("Invalid Card Number")
   end
 
