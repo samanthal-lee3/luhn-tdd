671875b65c5f3c98b36677ab82e7bffabc30d465
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-22 01:22:45 +0000

    Passing rspec tests for invalid data

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,51 +1,55 @@
  class Luhn
 
     #build an instance of the luhn class
-    def initialize(arr)
-      @luhn = arr
+    def initialize(input)
+      @input = input
+    end
+
+    def string_to_array
+      @input.chars.map {|e| e.to_i}
     end
 
     #build new array
     #store doubled val of each even index
     #store original val of each odd index
     def double_digits
-      @doubled = []
+      doubled = []
 
-      @luhn.each_with_index do |val, index|
-        if (index %2 == 0)
-          doubled_digit = val * 2
-          @doubled << doubled_digit
+      string_to_array.each_with_index do |val, index|
+        if index % 2 == 0
+          doubled << val * 2
         else
-          @doubled << val
+          doubled << val
         end
       end
+      doubled
     end
 
     #Build a new array. For each index...
     #if val>=10, subtract 9 and store new val
     #if val<10, store original val
     def compare_num
-      @subtracted = []
+      subtracted = []
 
-      @doubled.each_with_index do |val, index|
-        if val > 10
-          changed_num = val - 9
-          @subtracted << changed_num
+      double_digits.each_with_index do |val, index|
+        if val >= 10
+          subtracted << val - 9
         else
-          @subtracted << val
+          subtracted << val
         end
       end
+      subtracted
     end
     
     #sum total of @subtracted array
     #divide total by 10 and test for valid num
-    def test_validity
-     card_sum = @subtracted.sum
+    def valid?
+     card_sum = compare_num.sum
      if 
-        card_sum %10 == 0
-        puts "Success!"
+        card_sum % 10 == 0
+        true
      else
-        puts "Invalid Card Number- Try Again"
+        false
       end
     end
 
--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -5,49 +5,51 @@ describe Luhn do
  
   it "should take the value of a credit card" do
     #arrange
-    my_card = Luhn.new([4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
+    my_card = Luhn.new("4194560385008505")
 
     #act
-
+    results = my_card.string_to_array
     #assert
-    expect(my_card).to eq(my_card)
+    expect(results).to eq([4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
   end
 
-  it "should double the value of each even index" do
+  it "#double_digits should double the value of each even index" do
     #arrange
-    my_card = Luhn.new([4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
+    my_card = Luhn.new("4194560385008505")
 
     #act
-    my_card.double_digits
+    results = my_card.double_digits
 
     #assert
-    expect(my_card).to eq([8,1,18,4,10,6,0,3,16,5,0,0,16,5,0,5])
+    expect(results).to eq([8, 1, 18, 4, 10, 6, 0, 3, 16, 5, 0, 0, 16, 5, 0, 5])
   end
 
-  it "should adjust values if val>=10" do 
+  it "#compare_num should adjust values if val>=10" do 
     #arrange
-    my_card = Luhn.new([4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
+    my_card = Luhn.new("4194560385008505")
 
     #act
-    my_card.double_digits
-    my_card.compare_num
+    results = my_card.compare_num
 
     #assert
-   expect(my_card).to eq([8,1,9,4,1,6,0,3,7,5,0,0,7,5,0,5])
+   expect(results).to eq([8,1,9,4,1,6,0,3,7,5,0,0,7,5,0,5])
   end
 
-  it "should test for valid card number" do 
+  it "#valid? should test for valid card number" do 
     #arrange
-    my_card = Luhn.new([4,1,9,4,5,6,0,3,8,5,0,0,8,5,0,5])
+    my_card = Luhn.new("4194560385008505")
 
     #act
-    my_card.double_digits
-    my_card.compare_num
-    my_card.test_validity
+    result = my_card.valid?
 
     #assert
-    expect(my_card).to eq("Invalid Card Number")
+    expect(result).to eq(false)
   end
+
+  #another test for true test
+  #use a context within rspec- valid context or invalid context
+  #with valid arguments
+  #with invalid arguments
 
 end
     
